67648a944cbe7e7f5eb62f2593db590b8b896e08
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2022-02-04 16:01:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-function preload() {
+noseX = 0;
+noseY = 0;
 
+function preload() {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png', 'https://i.postimg.cc/PxFvYgkv/l1.png');
 }
 
 function setup() {
-    canvas = createCanvas(300, 300);
+    canvas = createCanvas(400, 400);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(300, 300);
     video.hide();
 
-    poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('pose', gotPoses);
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 
 function modelLoaded() {
-    console.log('PoseNet Is Initilalized');
+    console.log('Posenet Loaded');
 }
 
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
-        console.log("nose-x = " + results[0].pose.nose.x);
-        console.log("nose-y = " + results[0].pose.nose.y);
+        console.log("nose x= " + results[0].pose.nose.x);
+        console.log("nose y= " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x - 175;
+        noseY = results[0].pose.nose.y - 45;
     }
-
 }
 
 function draw() {
-    image(video, 0, 0, 300, 300);
+    image(video, 0, 0, 400, 400);
+    image(mustache, noseX, noseY, 60, 40);
 }
 
 function take_snapshot() {
